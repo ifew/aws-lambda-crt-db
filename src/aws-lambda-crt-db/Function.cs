29b62aee253481c9754539d52afc3aee71bccf03
iFew <chitpong@gmail.com>
2019-05-03 18:46:07 +0700

    Lean project by remove DI and using ADO.NET

--- src/aws-lambda-crt-db/Function.cs
+++ src/aws-lambda-crt-db/Function.cs
@@ -1,28 +1,21 @@
-using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.Json;
-using Microsoft.Extensions.DependencyInjection;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Net;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace aws_lambda_crt_db
 {
     public class Function
     {
-        private ServiceProvider _service;
-
-        public Function() : this (Bootstrap.CreateInstance()) {}
-
-        public Function(ServiceProvider service)
-        {
-            _service = service;
-        }
         private static async Task Main(string[] args)
 		{
-            Func<ILambdaContext, List<DistrictModel>> func = FunctionHandler;
+            Func<ILambdaContext, List<Member>> func = FunctionHandler;
 			using(var handlerWrapper = HandlerWrapper.GetHandlerWrapper(func, new JsonSerializer()))
 			{
 				using(var lambdaBootstrap = new LambdaBootstrap(handlerWrapper))
@@ -32,13 +25,61 @@ namespace aws_lambda_crt_db
 			}
 		}
         
-        public static List<DistrictModel> FunctionHandler(ILambdaContext context)
+        public static List<Member> FunctionHandler(ILambdaContext context)
         {
-            Function fn = new Function();
-            Services service = fn._service.GetService<Services>();
-            List<DistrictModel> districts = service.List_district();
+            Console.WriteLine("Log: Start Connection");
 
-            return districts;
+            string configDB = Environment.GetEnvironmentVariable("DB_CONNECTION");
+
+            using (var _connection = new MySqlConnection(configDB))
+            {
+                Console.WriteLine("Log: _connection.ConnectionString: " + _connection.ConnectionString);
+
+                if (_connection.State == ConnectionState.Closed)
+                    _connection.Open();
+
+                Console.WriteLine("Log: State: " + _connection.State.ToString());
+                Console.WriteLine("Log: DB ServerVersion: " + _connection.ServerVersion);
+
+                using (var cmd = new MySqlCommand("SELECT * FROM test_member", _connection))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        Console.WriteLine("Log: reader.FieldCount: " + reader.FieldCount);
+                        List<Member> members = new List<Member>();
+                        while (reader.Read())
+                        {
+                            members.Add(new Member()
+                            {
+                                Id = Convert.ToInt32(reader["id"]),
+                                Firstname = reader["firstname"].ToString(),
+                                Lastname = reader["lastname"].ToString(),
+                            });
+                        }
+
+                        Console.WriteLine("Log: Count: " + members.Count);
+
+                        return members;
+                    }
+                }
+
+            }
         }
+        
+    }
+
+    [Table("test_member")]
+    public class Member
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("firstname")]
+        public string Firstname { get; set; }
+
+        [Column("lastname")]
+        public string Lastname { get; set; }
+
     }
 }
